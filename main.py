d216236e7ed6a96be9de391941454e28e70de41a
AdershBabu <65764036+AdershBabu@users.noreply.github.com>
2022-08-09 20:40:41 +0530

    Create main.py
    Code for leapyear

---
+++ main.py
@@ -0,0 +1,14 @@
+while 1: #Loop operation for repeated action
+
+    print("Enter the year:")
+
+    year = int(input())# Getting year as input
+    if year % 4 == 0:#Checking the condition for leap year
+        {
+            print(year, " is a leap year")#result
+        }
+    else:
+        {
+            print(year, "is not a leap year")#result
+        }
+
